3babb9a758d2f598fd6be291132a33f2dcc9c35e
Shubham2003-jha <96909698+Shubham2003-jha@users.noreply.github.com>
2023-06-07 21:27:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+import sys
+import random
+import pygame
+from pygame.locals import *
+FPS=32
+SCREENHEIGHT=500
+SCREENWIDTH=300
+screen=pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+GROUND=SCREENHEIGHT*0.8
+GAME_SPRITES={}
+GAME_SOUNDS={}
+PLAYER='sprites/flappy.png'
+BACKGROUND='sprites/backgorund.png'
+PIPE='sprites/pipe.png'
+basex=0
